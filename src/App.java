f2bf1c377ccd8972da78c0d2b6470405c845e71e
fedora_user <fedora_user@endeavouros.intern>
2024-03-31 01:19:35 +0300

    update

--- src/App.java
+++ src/App.java
@@ -25,7 +25,7 @@ public class App {
             }
             option = scanner.nextInt();
             if (option > 3) {
-                System.out.println("Введите число от 1 до 3!");
+                System.out.println("Введите цифру от 1 до 3!");
                 continue;
             }
         } while (option < 1 || option > 4);
@@ -57,7 +57,7 @@ public class App {
                             System.out.println("Гиперболический синус " + hyperSinInput + " = " + hyperSinValue);
                             break;
                         default:
-                            System.out.println("Вы ввели неверный вариант!");
+                            System.out.println("Введите цифру от 1 до 3!");
                             break;
                     }
                     break;
@@ -90,6 +90,78 @@ public class App {
                             double hyperSinInput = scanner.nextInt();
                             double hyperSinValue = Math.cosh(Math.toRadians(hyperSinInput));
                             System.out.println("Гиперболический косинус " + hyperSinInput + " = " + hyperSinValue);
+                            break;
+                        default:
+                            System.out.println("Вы ввели неверный вариант!");
+                            break;
+                    }
+                    break;
+                } catch (InputMismatchException e) {
+                    System.out.println("Введите достоверные данные!");
+                }
+            case 3:
+                try {
+                    System.out.println("1 - тангенс");
+                    System.out.println("2 - арктангенс");
+                    System.out.println("3 - гиперболический тангенс");
+                    System.out.print("--Введите вариант: ");
+                    int tanInputOption = scanner.nextInt();
+                    switch (tanInputOption) {
+                        case 1:
+                            System.out.print("--Введите число для вычисления тангенса: ");
+                            double tanInput = scanner.nextInt();
+                            double tanValue = Math.tan(Math.toRadians(tanInput));
+                            System.out.println("Косинус " + tanInput + " = " + tanValue);
+                            break;
+                        case 2:
+                            System.out.print("--Введите число для вычисления арктангенса: ");
+                            double arcTanInput = scanner.nextInt();
+                            double arcTanValue = Math.atan(Math.toRadians(arcTanInput));
+                            System.out.println("Косинус " + arcTanInput + " = " + arcTanValue);
+                            break;
+                        case 3:
+                            System.out.print("--Введите число для вычисления гипкрболического тангенса: ");
+                            double hyperTanInput = scanner.nextInt();
+                            double hyperTanValue = Math.tanh(Math.toRadians(hyperTanInput));
+                            System.out.println("Косинус " + hyperTanInput + " = " + hyperTanValue);
+                            break;
+                        default:
+                            System.out.println("Вы ввели неверный вариант!");
+                            break;
+                    }
+                    break;
+                } catch (InputMismatchException e) {
+                    System.out.println("Введите достоверные данные!");
+                }
+            case 4:
+                try {
+                    System.out.println("1 - котангенс");
+                    System.out.println("2 - арккотангенс");
+                    System.out.println("3 - гиперболический котангенс");
+                    System.out.print("--Введите вариант: ");
+                    int cotanInputOption = scanner.nextInt();
+                    switch (cotanInputOption) {
+                        case 1:
+                            System.out.print("--Введите число для вычисления котангенса: ");
+                            double cotanInput = scanner.nextInt();
+                            double tanValue = Math.tan(Math.toRadians(cotanInput));
+                            double cotanValue = 1 / tanValue;
+                            System.out.println("Котангенс " + cotanInput + " = " + cotanValue);
+                            break;
+                        case 2:
+                            System.out.print("--Введите число для вычисления арккотангенса: ");
+                            double arcCotanInput = scanner.nextInt();
+                            double arcTanValue = Math.atan(Math.toRadians(arcCotanInput));
+                            double arcCotanValue = 1 / arcTanValue;
+                            System.out.println("Арккотангенс " + arcCotanInput + " = " + arcCotanValue);
+                            break;
+                        case 3:
+                            System.out.print("--Введите число для вычисления гиперболического котангенса: ");
+                            double hyperCotanInput = scanner.nextInt();
+                            double hyperTanValue = Math.tanh(Math.toRadians(hyperCotanInput));
+                            double hyperCotanValue = 1 / hyperTanValue;
+                            System.out
+                                    .println("Гиперболический котангенс " + hyperCotanInput + " = " + hyperCotanValue);
                             break;
                         default:
                             System.out.println("Вы ввели неверный вариант!");
